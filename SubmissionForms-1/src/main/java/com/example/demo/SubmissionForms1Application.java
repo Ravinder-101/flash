7d23d6b1bcd413c24b2330d24062455608af6787
Ravinder-101 <rs015102@gmail.com>
2021-02-06 23:18:43 +0530

    First Commit

---
+++ SubmissionForms-1/src/main/java/com/example/demo/SubmissionForms1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmissionForms1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmissionForms1Application.class, args);
+	}
+
+}
